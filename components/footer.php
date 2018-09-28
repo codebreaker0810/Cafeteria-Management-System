43259ac16c72b8c10dedcfa6284cd950785038d3
unknown <sanket7575757575@gmail.com>
2018-09-28 14:10:02 +0530

    new file:   components/carousal.html 	new file:   components/carousal.php 	new file:   components/css/slide.css 	new file:   components/css/style.css 	new file:   components/footer.php 	new file:   components/header.php 	new file:   components/hm.html 	new file:   components/images/1.jpg 	new file:   components/images/2.jpg 	new file:   components/images/20.jpg 	new file:   components/images/210129-min.jpg 	new file:   components/images/3.jpg 	new file:   components/images/30.jpg 	new file:   components/images/4.jpg 	new file:   components/images/80.jpg 	new file:   components/images/burgerp-min (1).jpg 	new file:   components/images/cafe.jpg 	new file:   components/images/cafelogo.png 	new file:   components/images/cafeteriabg.jpg 	new file:   components/images/download1.jpg 	new file:   components/images/hdb.jpg 	new file:   components/images/images (1).jpg 	new file:   components/images/images (2).jpg 	new file:   components/images/images (3).jpg 	new file:   components/images/images (4).jpg 	new file:   components/images/images (5).jpg 	new file:   components/images/images.png 	new file:   components/images/img.png 	new file:   components/images/logo.jpg 	new file:   components/images/off.jpg 	new file:   components/images/one.jpg 	new file:   components/images/pizza.jpg 	new file:   components/images/three.jpg 	new file:   components/images/two.jpg 	new file:   components/index.php 	new file:   components/sample.html

---
+++ components/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   padding: 20px;
+   background-color: #333;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <p>All rights reserved &copy MIT-COE 2018</p>
+
+</div>
+
+</body>
+</html> 
---
+++ components/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Cafeteria </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>	
+	<body>
+		
+	  <?php
+	  	include("header.php");
+	  	include("carousal.php");
+	  	include("footer.php");
+	  ?>
+
+</body>
+
+</html>    
+
+
+
